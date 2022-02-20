b14be9f3af9915c084284ad21ccb3ab045884a3e
santiDev <androidsan94@gmail.com>
2022-02-19 21:57:58 -0600

    Initial finish

---
+++ app/src/main/java/com/santidev/listviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.santidev.listviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_detail_layout);
+    }
+}
